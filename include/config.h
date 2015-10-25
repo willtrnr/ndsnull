16d76bd2e2117c5c88bb4a487a4ee6176121a82a
William Turner <willtur.will@gmail.com>
2015-10-25 18:31:29 -0400

    Configurability

--- include/config.h
+++ include/config.h
@@ -5,4 +5,7 @@
 
 #define MAX_BODY_SIZE 1024 * 1024
 
+#define BACKEND_HOST "192.168.1.27"
+#define BACKEND_PATH "/challenge"
+
 #endif
--- source/server.c
+++ source/server.c
@@ -146,7 +146,7 @@ void process_request(int sockfd, const char* method, const char* request, const 
 
             char* resp = NULL;
             int resplen = 0;
-            int status = http_request(POST, "127.0.0.1", "/challenge", buf, bodylen, &resp, &resplen);
+            int status = http_request(POST, BACKEND_HOST, BACKEND_PATH, buf, bodylen, &resp, &resplen);
 
             buf = (char*)realloc(buf, sizeof(PAYLOAD) + resplen);
             sprintf(buf, PAYLOAD, resp);
